e37313931262f03040cdbb215650904f00a706c8
Sameer Faridi <s.faridi007@gmail.com>
2023-04-19 23:15:50 +0530

    sameer

--- react_todo_app/src/App.jsx
+++ react_todo_app/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more and more and more . 😎 hii guys sameer faridi this side 
+      . 😎 hii guys sameer faridi this side 
       </p>
     </div>
   )
