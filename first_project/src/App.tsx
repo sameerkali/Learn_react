ccfe56d5f938d4b8b6b4df52847cb753c80d7202
Sameer Faridi <s.faridi007@gmail.com>
2023-04-26 23:08:23 +0530

    sameer

--- first_project/src/App.tsx
+++ first_project/src/App.tsx
@@ -1,9 +1,7 @@
 import Message from "./message";
 
 function App() {
-  return <div><Message></Message></div>
-  return <div><Message></Message></div>
-  return <div><Message></Message></div>
+  return <div><Message></Message></div> // yrr pareshan ho gayaa hu main apna time waste krte krte ab lagta hai ki kuchh krna hi hoga 
 }
 
 export default App
