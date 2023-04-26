91c8672fe0dd478462e15c19edae7d14740dde68
Sameer Faridi <s.faridi007@gmail.com>
2023-04-26 22:42:40 +0530

    sameer

--- first_project/src/App.tsx
+++ first_project/src/App.tsx
@@ -2,6 +2,8 @@ import Message from "./message";
 
 function App() {
   return <div><Message></Message></div>
+  return <div><Message></Message></div>
+  return <div><Message></Message></div>
 }
 
 export default App
