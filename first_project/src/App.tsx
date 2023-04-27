631e614274bbd64872c109fa1368e1f8d58bd366
Sameer Faridi <s.faridi007@gmail.com>
2023-04-27 21:04:57 +0530

    paid course by mosh

--- first_project/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import Message from "./message";
-
-function App() {
-  return <div><Message></Message></div> // yrr pareshan ho gayaa hu main apna time waste krte krte ab lagta hai ki kuchh krna hi hoga 
-}
-
-export default App
--- first_project/src/message.tsx
+++
@@ -1,4 +0,0 @@
-function Message() {
-return <h1>hello world</h1>;
-}
-export default Message;
