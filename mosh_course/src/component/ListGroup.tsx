f533c8924534145c0f1ff4b3c096e6c321c8c053
Sameer Faridi <s.faridi007@gmail.com>
2023-04-27 22:38:08 +0530

    check post 1  -  conditional rendring

--- mosh_course/src/component/ListGroup.tsx
+++ mosh_course/src/component/ListGroup.tsx
@@ -1,21 +1,31 @@
 function ListGroup() {
+  let items = [
+    "Ghaziabad",
+    "Patna",
+    "Delhi",
+    "Bhopal",
+    "Jaipur",
+    "Gurgao",
+    "Lucknow",
+    "Dehradune",
+    "Nenital"
+  ];
 
-    const items = [
-        'Ghaziabad',
-        'Patna',
-        'Delhi',
-        'Bhopal',
-        'Jaipur',
-        'Gurgao',
-        'Lucknow',
-        'Dehradune',
-        'Nenital'
-    ];
+  items = [];
+
+// const getMessage = (msg:string) => {                            method1
+//     return items.length === 0 ? <p>{msg}</p> : null;
+// }
 
   return (
     <>
+      <h1>List items</h1>
+      {items.length === 0 && <p> no item found</p>}                     
+      {/* {getMessage('No Cities')}                                     method1 */}
       <ul className="list-group">
-        {items.map(items => <li>{items}</li>)}
+        {items.map((items) => (
+          <li key={items}>{items}</li>
+        ))}
       </ul>
     </>
   );
