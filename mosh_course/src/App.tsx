4560831ffa06fd8ac9f97aa5e06934c6f2e95a3a
Sameer Faridi <s.faridi007@gmail.com>
2023-05-01 12:25:14 +0530

    start 1 may 23

--- mosh_course/src/App.tsx
+++ mosh_course/src/App.tsx
@@ -1,7 +1,6 @@
-import Message from "./Message";
-import ListGroup from "./component/ListGroup"
+import ListGroup from "./component/ListGroup";
 
-function App(){
+function App() {
   const items = [
     "Ghaziabad",
     "Patna",
@@ -13,97 +12,12 @@ function App(){
     "Dehradune",
     "Nenital"
   ];
-  return <div> <ListGroup items={items} heading="Cities"/> </div>;
+  return (
+    <div>
+      {" "}
+      <ListGroup items={items} heading="Cities" />{" "}
+    </div>
+  );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
