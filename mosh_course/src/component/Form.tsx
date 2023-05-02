9e8f4f199abe9e76e3e58bad9b834ea4b3bd5bed
Sameer Faridi <s.faridi007@gmail.com>
2023-05-02 23:59:29 +0530

    lets clean code

--- mosh_course/src/component/Form.tsx
+++ mosh_course/src/component/Form.tsx
@@ -1,10 +1,18 @@
-import { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
-const Form = () => {
-  const { register, handleSubmit, formState: { errors }} = useForm();
+interface FormData {
+  name: string;
+  age: number;
+}
 
-  const onSubmit = (data : FieldValues) => console.log(data)
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -13,14 +21,22 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", {required: true, minLength: 3, maxLength: 16})}
+          {...register("name", { required: true, minLength: 3, maxLength: 16 })}
           type="text"
           id="name"
           className="form-control"
         />
-        { errors.name?.type === 'required' && <p>The name field is require.</p>}
-        { errors.name?.type === 'minLength' && <p className="text-danger">The name must be more then 3 charactors.</p>}
-        { errors.name?.type === 'maxLength' && <p className="text-danger">The name must be less then 16 charactors.</p>}
+        {errors.name?.type === "required" && <p>The name field is require.</p>}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name must be more then 3 charactors.
+          </p>
+        )}
+        {errors.name?.type === "maxLength" && (
+          <p className="text-danger">
+            The name must be less then 16 charactors.
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
