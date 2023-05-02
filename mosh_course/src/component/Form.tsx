a19df74ad75d5b7d02d2578eb58b3e76fbe05462
Sameer Faridi <s.faridi007@gmail.com>
2023-05-02 19:19:50 +0530

    getting some errors

--- mosh_course/src/component/Form.tsx
+++ mosh_course/src/component/Form.tsx
@@ -1,33 +1,31 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, useState } from "react";
 
-const Form = () => {
-  const nameRef = useRef<HTMLInputElement>(null);
-  const ageRef = useRef<HTMLInputElement>(null);
-  const person = { name: " ", age: 0 };
 
   const handelSubmit = (event: FormEvent) => {
     event.preventDefault();
-
-    if (nameRef.current != null) person.name = nameRef.current.value;
-
-    if (ageRef.current != null) person.age = parseInt(ageRef.current.value);
-
-    console.log(person)
   };
+
+
+  const Form = () => {
+    const [person, setPerson] = useState({
+      name: '',
+      age: 0
+    })
+  }
 
   return (
     <form onSubmit={handelSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
-          Name type here
+          Name
         </label>
-        <input ref={nameRef} type="text" id="name" className="form-control" />
+        <input onChange={(event) => setPerson({ ...person ,name:event.target.value})} type="text" id="name" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input ref={ageRef} id="age" type="number" className="form-control" />
+        <input id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
@@ -35,5 +33,6 @@ const Form = () => {
     </form>
   );
 };
+
 
 export default Form;
