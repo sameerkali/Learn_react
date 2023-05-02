bfc8da26712f08451539c0b8955772e493648c89
Sameer Faridi <s.faridi007@gmail.com>
2023-05-02 23:25:22 +0530

    code for react-hook-form@7.43

--- mosh_course/src/component/Form.tsx
+++ mosh_course/src/component/Form.tsx
@@ -2,28 +2,16 @@ import { FormEvent, useState } from "react";
 import { useForm } from "react-hook-form";
 
 const Form = () => {
-
-  const [person, setPerson] = useState({
-    name: '',
-    age: ''
-  });
-
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
-  };
+  const { register, handleSubmit } = useForm();
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(data => console.log(data))}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
+          {...register("name")}
           type="text"
           id="name"
           className="form-control"
@@ -34,10 +22,7 @@ const Form = () => {
           Age
         </label>
         <input
-          onChange={(event) =>
-            setPerson({ ...person, age: parseInt(event.target.value) })
-          }
-          value={person.age}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
