9f9f4fab774971440aff31bbdc703461ec540e37
Sameer Faridi <s.faridi007@gmail.com>
2023-05-02 16:28:00 +0530

    getting the data from form

--- mosh_course/src/component/Form.tsx
+++ mosh_course/src/component/Form.tsx
@@ -1,11 +1,19 @@
-import React, {FormEvent} from "react";
+import React, {FormEvent, useRef} from "react";
+
 
 const Form = () => {
+
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
 
 
     const handelSubmit = (event : FormEvent) =>{
         event.preventDefault()
-        console.log('submitted')
+        if(nameRef.current != null)
+         console.log(nameRef.current.value)
+        if(ageRef.current != null)
+        console.log(ageRef.current.value)
+    
     }
 
 
@@ -15,11 +23,11 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input type="text" id="name" className="form-control" />
+        <input ref={nameRef} type="text" id="name" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">Age</label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">Submit</button>
     </form>
