c76f8da0771bb959ba5567617c25454e5a03085a
Sameer Faridi <s.faridi007@gmail.com>
2023-05-02 19:39:04 +0530

    fixing all the errors using chatGPT

--- mosh_course/src/component/Form.tsx
+++ mosh_course/src/component/Form.tsx
@@ -1,31 +1,43 @@
-import React, { FormEvent, useState } from "react";
+import { FormEvent, useState } from "react";
 
+const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: 0
+  });
 
-  const handelSubmit = (event: FormEvent) => {
+  const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
+    console.log(person);
   };
 
-
-  const Form = () => {
-    const [person, setPerson] = useState({
-      name: '',
-      age: 0
-    })
-  }
-
   return (
-    <form onSubmit={handelSubmit}>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input onChange={(event) => setPerson({ ...person ,name:event.target.value})} type="text" id="name" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          type="text"
+          id="name"
+          className="form-control"
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, age: parseInt(event.target.value) })
+          }
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
@@ -33,6 +45,5 @@ import React, { FormEvent, useState } from "react";
     </form>
   );
 };
-
 
 export default Form;
