22250314997dec13154b1c5188c27e00a7b22b09
Sameer Faridi <s.faridi007@gmail.com>
2023-05-04 12:54:33 +0530

    first part of expense tracker app is compleate learning- []filter,[]reduce methods

--- mosh_course/src/App.tsx
+++ mosh_course/src/App.tsx
@@ -1,61 +1,22 @@
+import { useState } from "react";
 import Table from "./eexpense-tracker/components/table";
 
 function App() {
-  const expences = [
-    {id: 1, description: 'sameer', amount: 10, category: 'Utilitys'},
-    {id: 2, description: 'faridi', amount: 10, category: 'Utilitys'},
-    {id: 3, description: 'rais', amount: 10, category: 'Utilitys'},
-    {id: 4, description: 'faridi', amount: 10, category: 'Utilitys'},
-  ]
- 
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "sameer", amount: 10, category: "Utilitys" },
+    { id: 2, description: "faridi", amount: 10, category: "Utilitys" },
+    { id: 3, description: "rais", amount: 10, category: "Utilitys" },
+    { id: 4, description: "faridi", amount: 10, category: "Utilitys" }
+  ]);
+
   return (
     <div>
-    <Table expenses={expences} onDelete={(id) => console.log('Delete', id)} />
+      <Table
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import ListGroup from "./component/ListGroup/ListGroup";
-
-// function App() {
-//   const items = [
-//     "Ghaziabad",
-//     "Patna",
-//     "Delhi",
-//     "Bhopal",
-//     "Jaipur",
-//     "Gurgao",
-//     "Lucknow",
-//     "Dehradune",
-//     "Nenital"
-//   ];
-//   const handleSelectItem = (item: string) => {
-//     console.log(item);
-//   }
-//   return (
-//     <div>
-//       <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
-//     </div>
-//   );
-// }
-
-// export default App;
--- mosh_course/src/eexpense-tracker/components/table.tsx
+++ mosh_course/src/eexpense-tracker/components/table.tsx
@@ -11,6 +11,7 @@ interface Props {
 }
 
 const table = ({ expenses, onDelete }: Props) => {
+  if(expenses.length === 0) return null;
   return (
     <div className="table">
       <table className="table table-bordered">
